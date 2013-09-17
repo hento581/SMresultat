26e7a55c00f4d24d16fbe324c5216c69352e1232
hento581 <henrik.tosteberg@gmail.com>
2013-09-17 16:42:14 +0200

    La in lite resultat

--- resultat.php
+++ resultat.php
@@ -29,6 +29,96 @@
 					<a href="<?php echo $winsplits ?>5253">Stafett 2005</a><br>
 				<?php endif; ?>
 				<?php
+				 if($_GET["id"] === "2") : ?>  
+					<a href=" <?php echo $winsplits ?>22926">Lång 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574">Lång 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075">Lång 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743">Lång 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412">Lång 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419">Lång 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663">Lång 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173">Lång 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696">Klassisk 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841">Klassisk 2003</a><br>
+					<a href="<?php echo $winsplits ?>1983">Klassisk 2002</a><br>
+					<a href="<?php echo $winsplits ?>1280">Klassisk 2001</a><br>
+					<a href="<?php echo $winsplits ?>610">Klassisk 2000</a><br>
+					<a href="<?php echo $winsplits ?>207">Klassisk 1999</a><br>
+					<a href="<?php echo $winsplits ?>39">Klassisk 1998</a><br>
+				<?php endif; ?>
+				<?php
+				 if($_GET["id"] === "3") : ?>  
+					<a href=" <?php echo $winsplits ?>22926">Medel 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574">Medel 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075">Medel 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743">Medel 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412">Medel 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419">Medel 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663">Medel 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173">Medel 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696">Kort 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841">Kort 2003</a><br>
+					<a href="<?php echo $winsplits ?>1983">Kort 2002</a><br>
+					<a href="<?php echo $winsplits ?>1280">Kort 2001</a><br>
+					<a href="<?php echo $winsplits ?>610">Kort 2000</a><br>
+					<a href="<?php echo $winsplits ?>207">Kort 1999</a><br>
+					<a href="<?php echo $winsplits ?>39">Kort 1998</a><br>
+				<?php endif; ?>
+				<?php
+				 if($_GET["id"] === "4") : ?>  
+					<a href=" <?php echo $winsplits ?>22926">Sprint 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574">Sprint 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075">Sprint 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743">Sprint 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412">Sprint 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419">Sprint 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663">Sprint 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173">Sprint 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696">Sprint 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841">Sprint 2003</a><br>
+					
+				<?php endif; ?>
+				<?php
+				 if($_GET["id"] === "5") : ?>  
+					<a href=" <?php echo $winsplits ?>22926">Natt 2013</a><br>
+					<a href=" <?php echo $winsplits ?>22926">Natt 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574">Natt 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075">Natt 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743">Natt 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412">Natt 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419">Natt 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663">Natt 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173">Natt 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696">Natt 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841">Natt 2003</a><br>
+					<a href="<?php echo $winsplits ?>1983">Natt 2002</a><br>
+					<a href="<?php echo $winsplits ?>1280">Natt 2001</a><br>
+					<a href="<?php echo $winsplits ?>610">Natt 2000</a><br>
+					<a href="<?php echo $winsplits ?>207">Natt 1999</a><br>
+					<a href="<?php echo $winsplits ?>39">Natt 1998</a><br>
+					<a href="<?php echo $winsplits ?>4">Natt 1997</a><br>
+				<?php endif; ?>
+				<?php
+				 if($_GET["id"] === "6") : ?>  
+					<a href=" <?php echo $winsplits ?>22926">Ultra 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574">Ultra 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075">Ultra 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743">Ultra 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412">Ultra 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419">Ultra 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663">Ultra 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173">Ultra 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696">Ultra 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841">Ultra 2003</a><br>
+					<a href="<?php echo $winsplits ?>1983">Ultra 2002</a><br>
+					<a href="<?php echo $winsplits ?>1280">Ultra 2001</a><br>
+					<a href="<?php echo $winsplits ?>610">Ultra 2000</a><br>
+					<a href="<?php echo $winsplits ?>207">Ultra 1999</a><br>
+					<a href="<?php echo $winsplits ?>39">Ultra 1998</a><br>
+					<a href="<?php echo $winsplits ?>39">Ultra 1997</a><br>
+				<?php endif; ?>
+				
+				<?php
 			?>
 	
 	
