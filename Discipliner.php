b927d4cf516f09276c75171f1950367323d4eaaf
oskda283 <oskar_daniels@hotmail.com>
2013-09-17 17:52:05 +0200

    Fixat lite "design".

--- Discipliner.php
+++ Discipliner.php
@@ -1,12 +1,145 @@
 <div>
 	<div id="Discipliner" class="discipliner">
+		<?php
+		$winsplits = 'http://obasen.orientering.se/winsplits/online/sv/default.asp?page=classes&databaseId=';
+		?>
 		
-				<a href="resultat.php?id=1" >Stafett </a><br>
-				<a href="resultat.php?id=2" >Lång </a><br>
-				<a href="resultat.php?id=3" >Medel </a><br>
-				<a href="resultat.php?id=4" >Sprint </a><br>
-				<a href="resultat.php?id=5" >Natt </a><br>
-				<a href="resultat.php?id=6" >Ultra </a><br>	
+		<h1> Resultat från SM i orientering sedan 1997 </h1>
+		
+				<a href="?id=1" >Stafett</a><br>
+			
+			<?php
+			if (isset($_GET["id"])) {
+				 if($_GET["id"] === "1") : ?>  
+				 
+					<a href=" <?php echo $winsplits ?>23041"  target="_blank"> - 2012</a><br>
+					<a href="<?php echo $winsplits ?>18660"  target="_blank"> - 2011</a><br>
+					<a href="<?php echo $winsplits ?>15161"  target="_blank"> - 2010</a><br>
+					<a href="<?php echo $winsplits ?>12671"  target="_blank"> - 2009</a><br>
+					<a href="<?php echo $winsplits ?>10482"  target="_blank"> - 2008</a><br>
+					<a href="<?php echo $winsplits ?>8474"  target="_blank"> - 2007</a><br>
+					<a href="<?php echo $winsplits ?>6718"  target="_blank"> - 2006</a><br>
+					<a href="<?php echo $winsplits ?>5253"  target="_blank"> - 2005</a><br>
+			
+			<?php endif; }?>
+				
+				<a href="?id=2" >Lång </a><br>
+				
+			<?php
+				if (isset($_GET["id"])) {
+				if($_GET["id"] === "2") : ?> 
+					
+					<a href=" <?php echo $winsplits ?>22926" target="_blank">- 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574" target="_blank">- 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075" target="_blank">- 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743" target="_blank">- 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412" target="_blank">- 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419" target="_blank">- 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663" target="_blank">- 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173" target="_blank">- 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696" target="_blank">- 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841" target="_blank">- 2003</a><br>
+					<a href="<?php echo $winsplits ?>1983" target="_blank">- 2002</a><br>
+					<a href="<?php echo $winsplits ?>1280" target="_blank">- 2001</a><br>
+					<a href="<?php echo $winsplits ?>610" target="_blank">- 2000</a><br>
+					<a href="<?php echo $winsplits ?>207" target="_blank">- 1999</a><br>
+					<a href="<?php echo $winsplits ?>39" target="_blank">- 1998</a><br>
+				
+			<?php endif; }?>
+				
+				<a href="?id=3" >Medel </a><br>
+			
+			<?php
+				if (isset($_GET["id"])) {
+				if($_GET["id"] === "3") : ?> 
+				
+					<a href=" <?php echo $winsplits ?>22926" target="_blank">- 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574" target="_blank">- 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075" target="_blank">- 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743" target="_blank">- 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412" target="_blank">- 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419" target="_blank">- 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663" target="_blank">- 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173" target="_blank">- 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696" target="_blank">- 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841" target="_blank">- 2003</a><br>
+					<a href="<?php echo $winsplits ?>1983" target="_blank">- 2002</a><br>
+					<a href="<?php echo $winsplits ?>1280" target="_blank">- 2001</a><br>
+					<a href="<?php echo $winsplits ?>610" target="_blank">- 2000</a><br>
+					<a href="<?php echo $winsplits ?>207" target="_blank">- 1999</a><br>
+					<a href="<?php echo $winsplits ?>39" target="_blank">- 1998</a><br>
+			
+			<?php endif; }?>
+				
+				<a href="?id=4" >Sprint </a><br>
+				
+				<?php
+				if (isset($_GET["id"])) {
+				if($_GET["id"] === "4") : ?> 
+				
+					<a href=" <?php echo $winsplits ?>22926" target="_blank">- 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574" target="_blank">- 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075" target="_blank">- 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743" target="_blank">- 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412" target="_blank">- 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419" target="_blank">- 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663" target="_blank">- 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173" target="_blank">- 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696" target="_blank">- 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841" target="_blank">- 2003</a><br>
+			
+			<?php endif; }?>
+			
+				<a href="?id=5" >Natt </a><br>
+				
+				<?php
+				if (isset($_GET["id"])) {
+				if($_GET["id"] === "5") : ?> 
+				
+					<a href=" <?php echo $winsplits ?>22926" target="_blank">- 2013</a><br>
+					<a href=" <?php echo $winsplits ?>22926" target="_blank">- 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574" target="_blank">- 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075" target="_blank">- 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743" target="_blank">- 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412" target="_blank">- 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419" target="_blank">- 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663" target="_blank">- 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173" target="_blank">- 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696" target="_blank">- 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841" target="_blank">- 2003</a><br>
+					<a href="<?php echo $winsplits ?>1983" target="_blank">- 2002</a><br>
+					<a href="<?php echo $winsplits ?>1280" target="_blank">- 2001</a><br>
+					<a href="<?php echo $winsplits ?>610" target="_blank">- 2000</a><br>
+					<a href="<?php echo $winsplits ?>207" target="_blank">- 1999</a><br>
+					<a href="<?php echo $winsplits ?>39" target="_blank">- 1998</a><br>
+					<a href="<?php echo $winsplits ?>4" target="_blank">- 1997</a><br>
+			
+			<?php endif; }?>
+				
+				<a href="?id=6" >Ultralång </a><br>	
+				
+				<?php
+				if (isset($_GET["id"])) {
+				if($_GET["id"] === "6") : ?> 
+				
+					<a href=" <?php echo $winsplits ?>22926">- 2012</a><br>
+					<a href="<?php echo $winsplits ?>18574">- 2011</a><br>
+					<a href="<?php echo $winsplits ?>15075">- 2010</a><br>
+					<a href="<?php echo $winsplits ?>12743">- 2009</a><br>
+					<a href="<?php echo $winsplits ?>10412">- 2008</a><br>
+					<a href="<?php echo $winsplits ?>8419">- 2007</a><br>
+					<a href="<?php echo $winsplits ?>6663">- 2006</a><br>
+					<a href="<?php echo $winsplits ?>5173">- 2005</a><br>
+					<a href="<?php echo $winsplits ?>3696">- 2004</a><br>
+					<a href="<?php echo $winsplits ?>2841">- 2003</a><br>
+					<a href="<?php echo $winsplits ?>1983">- 2002</a><br>
+					<a href="<?php echo $winsplits ?>1280">- 2001</a><br>
+					<a href="<?php echo $winsplits ?>610">- 2000</a><br>
+					<a href="<?php echo $winsplits ?>207">- 1999</a><br>
+					<a href="<?php echo $winsplits ?>39">- 1998</a><br>
+					<a href="<?php echo $winsplits ?>39">- 1997</a><br>
+			
+			<?php endif; }?>
 						
 	</div>
 	
